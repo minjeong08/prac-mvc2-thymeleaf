e475c98520da1b040e952b1f6ad8db6d8f738e96
minnjung0887921 <minjeong0887921@gmail.com>
2024-03-16 14:46:27 +0900

    주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -132,4 +132,10 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
 }
